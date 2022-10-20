2b7bb481ff91fca7c9889258609a015b02eb21a7
jairoalperez <jairoalberto98@hotmail.com>
2022-10-20 17:23:43 -0400

    dashboard ready, estoy volando

---
+++ TReviews/src/app/screens/dashboard/dashboard.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  movies = []
+  IMG_URL = 'https://image.tmdb.org/t/p/w500'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+
+    this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=91355432bcd29434c686a0d24d0947fd')
+    .subscribe(res => {
+      this.movies = res.results;
+      console.log('Conexion con TMDB');
+    })
+
+  }
+
+}
---
+++ TReviews/src/app/screens/movie/movie.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.page.html',
+  styleUrls: ['./movie.page.scss'],
+})
+export class MoviePage implements OnInit {
+
+  movieid: string
+  API_KEY = '?api_key=91355432bcd29434c686a0d24d0947fd'
+  URL_API = 'https://api.themoviedb.org/3/movie/'
+  IMG_URL = 'https://image.tmdb.org/t/p/w500'
+  movie;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.movieid = this.activatedRoute.snapshot.paramMap.get('id')
+    this.http.get(this.URL_API+this.movieid+this.API_KEY)
+      .subscribe(res => this.movie = res)
+    
+  }
+
+}
