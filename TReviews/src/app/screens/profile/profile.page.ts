e0f456323db8a86b354d03ece13c4233aa1edc66
jairoalperez <jairoalberto98@hotmail.com>
2022-10-25 02:02:56 -0400

    casi todas las pages listas falta conectar

--- TReviews/src/app/screens/profile/profile.page.ts
+++ TReviews/src/app/screens/profile/profile.page.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  resenas: Array<any> = [
+    {autor: 'jairo', pelicula: 'avengers', contenido: 'esta pelicula es buenarda', fecha: '08/10/2022', puntaje: 10},
+    {autor: 'axel', pelicula: 'avengers', contenido: 'verga pa mala', fecha: '08/12/2022', puntaje: 3},
+    {autor: 'adnan', pelicula: 'avengers', contenido: 'masomenos', fecha: '08/12/2022', puntaje: 6},
+    {autor: 'acurero', pelicula: 'avengers', contenido: 'estan raspados todos', fecha: '10/31/2022', puntaje: 1},
+  ]
+
   constructor() { }
 
   ngOnInit() {
